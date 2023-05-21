c7b8af1852f17da91063ce775b749e8f23ec117c
Anderson Reis <andersonreis1234555@gmail.com>
2023-05-21 18:54:22 -0300

    finalizado

--- js/menu.js
+++ js/menu.js
@@ -5,9 +5,7 @@ var paredeFechar = document.querySelector(".paredeFechar");
 var linkMenu1 = document.querySelector(".linkMenu1");
 var linkMenu2 = document.querySelector(".linkMenu2");
 var linkMenu3 = document.querySelector(".linkMenu3");
-var linkMenu4 = document.querySelector(".linkMenu4");
-var linkMenu5 = document.querySelector(".linkMenu5");
-var linkMenu6 = document.querySelector(".linkMenu6");
+
 btn.addEventListener('click', () => {
     menu.classList.toggle('active');
     paredeFechar.classList.toggle('active');
@@ -39,18 +37,6 @@ linkMenu2.addEventListener('click', () => {
 
 })
 linkMenu3.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    paredeFechar.classList.toggle('active');
-
-
-})
-linkMenu4.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    paredeFechar.classList.toggle('active');
-
-
-})
-linkMenu5.addEventListener('click', () => {
     menu.classList.toggle('active');
     paredeFechar.classList.toggle('active');
 
